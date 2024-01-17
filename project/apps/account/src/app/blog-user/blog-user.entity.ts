022eeb7467330d771c96ccdeb35f915d6cb7a917
Igor Zorin <zorin.igor.jaroslavovich@gmail.com>
2024-01-17 19:08:49 +0700

    add comparePassword method

--- project/apps/account/src/app/blog-user/blog-user.entity.ts
+++ project/apps/account/src/app/blog-user/blog-user.entity.ts
@@ -1,6 +1,6 @@
 import { AuthUser } from '@project/shared/app/shared-types';
 import { Entity } from '@project/shared/core';
-import { genSalt, hash } from 'bcrypt';
+import { compare, genSalt, hash } from 'bcrypt';
 import { SALT_ROUNDS } from './blog-user.constant';
 
 export class BlogUserEntity implements AuthUser, Entity<string> {
@@ -46,5 +46,9 @@ export class BlogUserEntity implements AuthUser, Entity<string> {
     const salt = await genSalt(SALT_ROUNDS);
     this.passwordHash = await hash(password, salt);
     return this;
+  }
+
+  public async comparePassword(password: string): Promise<boolean> {
+    return compare(password, this.passwordHash);
   }
 }
