bee562515ab55d21aa705bab752116c9382b3bf3
Igor Zorin <zorin.igor.jaroslavovich@gmail.com>
2024-01-17 17:12:49 +0700

    add BlogUserEntity

---
+++ project/apps/account/src/app/app/blog-user/blog-user.entity.ts
@@ -0,0 +1,42 @@
+import { AuthUser } from '@project/shared/app/shared-types';
+import { Entity } from '@project/shared/core';
+
+export class BlogUserEntity implements AuthUser, Entity<string> {
+  public id: string;
+  public email: string;
+  public firstName: string;
+  public lastName: string;
+  public avatar?: string;
+  public registartionDate: string;
+  public postsNumber: number;
+  public subscribers: number;
+  public passwordHash: string;
+
+  constructor(user: AuthUser) {
+    this.populate(user);
+  }
+
+  public toPOJO() {
+    return {
+      id: this.id,
+      email: this.email,
+      firstName: this.firstName,
+      lastName: this.lastName,
+      avatar: this.avatar,
+      registartionDate: this.registartionDate,
+      postsNumber: this.postsNumber,
+      subscribers: this.subscribers,
+      passwordHash: this.passwordHash,
+    };
+  }
+
+  public populate(data: AuthUser): void {
+    this.email = data.email;
+    this.firstName = data.firstName;
+    this.lastName = data.lastName;
+    this.avatar = data.avatar;
+    this.registartionDate = data.registartionDate;
+    this.postsNumber = data.postsNumber;
+    this.subscribers = data.subscribers;
+  }
+}
