5f4128565b89a89164380f7b7d389d2445e2b2ea
Igor Zorin <zorin.igor.jaroslavovich@gmail.com>
2024-01-17 19:21:19 +0700

    add and register BlogUserRepository

--- project/apps/account/src/app/blog-user/blog-user.module.ts
+++ project/apps/account/src/app/blog-user/blog-user.module.ts
@@ -1,4 +1,8 @@
 import { Module } from '@nestjs/common';
+import { BlogUserRepository } from './blog-user.repository';
 
-@Module({})
+@Module({
+  exports: [BlogUserRepository],
+  providers: [BlogUserRepository],
+})
 export class BlogUserModule {}
