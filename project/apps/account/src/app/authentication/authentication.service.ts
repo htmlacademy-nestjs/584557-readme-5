66091edead5726207d65b7b1e2b43a4efaaf11b2
Igor Zorin <zorin.igor.jaroslavovich@gmail.com>
2024-01-17 19:25:36 +0700

    inject BlogUserRepository in AuthenticationService

--- project/apps/account/src/app/authentication/authentication.service.ts
+++ project/apps/account/src/app/authentication/authentication.service.ts
@@ -1,4 +1,7 @@
+import { BlogUserRepository } from './../blog-user/blog-user.repository';
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AuthenticationService {}
+export class AuthenticationService {
+  constructor(private readonly blogUserRepository: BlogUserRepository) {}
+}
