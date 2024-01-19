1e4c2fc808ad8c574dc003b563c8f0130dfa53a6
Igor Zorin <zorin.igor.jaroslavovich@gmail.com>
2024-01-19 12:35:21 +0700

    add /register route listener

--- project/apps/account/src/app/authentication/authentication.controller.ts
+++ project/apps/account/src/app/authentication/authentication.controller.ts
@@ -1,7 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthenticationService } from './authentication.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UserRdo } from './rdo/user.rdo';
+import { fillDto } from '@project/shared/helpers';
 
 @Controller('auth')
 export class AuthenticationController {
   constructor(private readonly authService: AuthenticationService) {}
+
+  @Post('register')
+  public async create(@Body() dto: CreateUserDto) {
+    const newUser = await this.authService.register(dto);
+
+    return fillDto(UserRdo, newUser.toPOJO());
+  }
 }
